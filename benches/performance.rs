364003884ac28fa7ac0350177d7b9880c24ba835
Gerald Schoiber <gerald.schoiber@ins.jku.at>
2022-08-28 12:43:07 +0200

    add new feature as well

--- benches/performance.rs
+++ benches/performance.rs
@@ -1,64 +1,27 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use dual_access_ringbuffer::*;
-
-fn read_buffer() {
-    const DATA: &[u8] = b"hello world";
-
-    ProducerToken::new(|mut producer| {
-        ConsumerToken::new(|mut consumer| {
-            let buffer = RingBuffer::<64>::new();
-            buffer.write_all(&mut producer, DATA).unwrap();
-
-            // read the whole buffer
-            let mut buf = [0u8; DATA.len()];
-            buffer.read_exact(&mut consumer, &mut buf).unwrap();
-        });
-    });
-}
-
-fn multi_read_buffer() {
-    const DATA: &[u8] = b"hello world";
-
-    ProducerToken::new(|mut producer| {
-        ConsumerToken::new(|consumer| {
-            let buffer = RingBuffer::<64>::new();
-            buffer.write_all(&mut producer, DATA).unwrap();
-
-            // read the whole buffer
-            for _ in 0..100 {
-                let mut buf = [0u8; DATA.len()];
-                buffer.read(&consumer, &mut buf).unwrap();
-            }
-        });
-    });
-}
+use dichotomy::*;
 
 fn async_multi_rw_1m() {
     const BUF_SIZE: usize = 1024;
     const DATA: &[u8] = b"hello world";
     const ITERATIONS: usize = 1_000_000;
 
-    ProducerToken::new(|mut producer| {
-        ConsumerToken::new(|mut consumer| {
-            let buffer = RingBuffer::<BUF_SIZE>::new();
+    let (mut producer, mut consumer) = Buffer::<BUF_SIZE>::new();
+    std::thread::scope(|scope| {
+        // producer
+        scope.spawn(|| {
+            for _ in 0..ITERATIONS {
+                producer.write_blocking(DATA).unwrap()
+            }
+        });
 
-            std::thread::scope(|scope| {
-                // producer
-                scope.spawn(|| {
-                    for _ in 0..ITERATIONS {
-                        buffer.write_all(&mut producer, DATA).unwrap()
-                    }
-                });
-
-                // consumer
-                scope.spawn(|| {
-                    let mut buf = [0u8; DATA.len()];
-                    for _ in 0..ITERATIONS {
-                        buffer.read_exact(&mut consumer, &mut buf).unwrap();
-                        assert!(DATA == buf)
-                    }
-                });
-            });
+        // consumer
+        scope.spawn(|| {
+            let mut buf = [0u8; DATA.len()];
+            for _ in 0..ITERATIONS {
+                consumer.read_blocking(&mut buf).unwrap();
+                assert!(DATA == buf)
+            }
         });
     });
 }
@@ -67,15 +30,16 @@ fn criterion_benchmark(c: &mut Criterion) {
     {
         let mut group = c.benchmark_group("sample size of 100");
         group.sample_size(100);
-        group.bench_function("read-buffer", |b| b.iter(|| read_buffer()));
-        group.bench_function("multi-read-buffer", |b| b.iter(|| multi_read_buffer()));
         group.bench_function("async-rw-1m", |b| b.iter(|| async_multi_rw_1m()));
     }
     {
         let mut group = c.benchmark_group("sample size of 500");
         group.sample_size(500);
-        group.bench_function("read-buffer", |b| b.iter(|| read_buffer()));
-        group.bench_function("multi-read-buffer", |b| b.iter(|| multi_read_buffer()));
+        group.bench_function("async-rw-1m", |b| b.iter(|| async_multi_rw_1m()));
+    }
+    {
+        let mut group = c.benchmark_group("sample size of 5000");
+        group.sample_size(5_000);
         group.bench_function("async-rw-1m", |b| b.iter(|| async_multi_rw_1m()));
     }
 }
